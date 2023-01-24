6de17c8cc04ef376ce3edf48c22272ac8bcd2c0c
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2023-01-24 19:41:29 -0300

    Adicionando função para aguardar as fonts carregar

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { useFonts } from 'expo-font'
 import { Header } from './src/components/Header'
 import { Login } from './src/Pages/Login'
 import { Home } from './src/Pages/Home'
-
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
@@ -15,6 +16,19 @@ export default function App() {
     'montserratItalic': require('./assets/fonts/Montserrat-Italic-VariableFont_wght.ttf'),
     'notoSerif': require('./assets/fonts/NotoSerif-Regular.ttf'),
   })
+
+  useEffect(() => {
+    function prepare() {
+      SplashScreen.preventAutoHideAsync
+    }
+    prepare()
+  }, [])
+
+  if (!loaded) {
+    return undefined
+  } else {
+    SplashScreen.hideAsync()
+  }
 
   const Stack = createNativeStackNavigator()
 
